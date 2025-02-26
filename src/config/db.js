62b13b83f2e0c81d1de13893fa5b9d46afd8352e
unknown <za0162hid@gmail.com>
2025-02-26 09:40:43 +0000

    rebuild

--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,9 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Make sure this is at the top of the file
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI;
-
-    // Log Mongo URI to check if it's correctly loaded
-    console.log("Mongo URI: ", mongoURI);
-
-    if (!mongoURI) {
-      console.error("MONGODB_URI is not defined in environment variables");
-      process.exit(1); // Exit the app if Mongo URI is not defined
-    }
-
-    const conn = await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    const conn = await mongoose.connect(process.env.URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
