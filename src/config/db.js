d26894120f88af9ab21adbfe3513c15cd7abf2c8
unknown <za0162hid@gmail.com>
2025-02-26 09:25:09 +0000

    db

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config(); // Make sure this is at the top of the file
 
 const connectDB = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI;
 
-    // Log Mongo URI for confirmation
+    // Log Mongo URI to check if it's correctly loaded
     console.log("Mongo URI: ", mongoURI);
 
     if (!mongoURI) {
       console.error("MONGODB_URI is not defined in environment variables");
-      process.exit(1);
+      process.exit(1); // Exit the app if Mongo URI is not defined
     }
 
-    // Connect to MongoDB without deprecated options
-    const conn = await mongoose.connect(mongoURI);
+    const conn = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
